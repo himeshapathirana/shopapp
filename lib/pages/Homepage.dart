1e5e6740586e03915b2e9a00003274cde09283f2
himeshapathirana <pathiranathimesha2021@gmail.com>
2024-12-25 15:40:54 +0530

    home page

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shopapp/components/groceryItemtile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,66 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final DateTime currentTime = DateTime.now();
+    final String greeting = currentTime.hour < 12
+        ? "Good Morning"
+        : currentTime.hour < 17
+            ? "Good Afternoon"
+            : "Good Evening";
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                greeting, // Display the greeting
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Let's make the order",
+                style: GoogleFonts.notoSerif(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(),
+            ),
+            const SizedBox(height: 24),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Fresh Items",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+
+            Expanded(
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile();
+                  }),
+            ),
+            // Add more widgets here if needed
+          ],
+        ),
+      ),
+    );
   }
 }
